0e1377de074464b11fe6deaad1ab0f8cb7dca44d
paweldurkalec <pdurkalec1006@gmail.com>
2022-11-22 22:15:43 +0100

    SSL works, use https only!

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,11 @@
 FROM prestashop/prestashop:1.7.7.8
 
+RUN chown -R www-data:root ./
+
 COPY ssl/ /etc/apache2/sites-available
 
 RUN a2enmod ssl
-RUN a2enconf ssl-params
-RUN a2ensite default-ssl
 RUN service apache2 restart
 
-EXPOSE 80
 EXPOSE 443
 
---
+++ webshop/modules/sendinblue/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'prestashop/sendinblue',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'dcf7e444fc1a2180870370f1c8c86e5b57a3d41a',
+        'type' => 'prestashop-module',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'prestashop/sendinblue' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'dcf7e444fc1a2180870370f1c8c86e5b57a3d41a',
+            'type' => 'prestashop-module',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
